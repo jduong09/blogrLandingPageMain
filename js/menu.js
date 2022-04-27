09fd8451448a24352ae4ad696bc5c821a00efbce
Justin Duong <jduong@umich.edu>
2022-04-27 12:51:29 -0400

    Removed margin used in margin for mobile designs when in desktop. Added javascript so only one sublist is open at a time.

--- js/menu.js
+++ js/menu.js
@@ -28,8 +28,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const sublistName = iconArrowEle.ariaLabel.split('-').at(-1);
     iconArrowEle.addEventListener('click', () => {
       const sublist = document.getElementById(`sublist-${sublistName}`);
+      const allArrowIcons = document.getElementsByClassName('icon-arrow');
+
       if (iconArrowEle.classList.contains('sublist-closed')) {
         sublist.classList.remove('hide');
+        
+        for (let j = 0; j < allArrowIcons.length; j++) {
+          if (allArrowIcons[j].classList.contains('sublist-opened')) {
+            const sublistName = allArrowIcons[j].ariaLabel.split('-').at(-1);
+            const sublist = document.getElementById(`sublist-${sublistName}`);
+            sublist.classList.add('hide');
+            allArrowIcons[j].classList.remove('sublist-opened');
+            allArrowIcons[j].classList.add('sublist-closed');
+          }
+        }
+
         iconArrowEle.classList.remove('sublist-closed');
         iconArrowEle.classList.add('sublist-opened');
       } else {
